dc0b7b08db3dd9b0960745c61b65dac0b22ad1e8
michaeldriscoll <michaelndriscoll81@gmail.com>
2019-12-02 16:15:10 -0500

    IT WORKS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var delay
 function clearBoard() {
-    delay = setInterval(resetBoard, 1200);
+    delay = setInterval(resetBoard, 2500);
+    return;
     }
 function resetBoard() {
     document.querySelectorAll(".square")[0].style.background = "white"
@@ -12,18 +13,9 @@ function resetBoard() {
     document.querySelectorAll(".square")[6].style.background = "white"
     document.querySelectorAll(".square")[7].style.background = "white"
     document.querySelectorAll(".square")[8].style.background = "white"
+    location.reload()
+    return;
 }
-document.querySelector(".resetButton").addEventListener('click', function(){
-                        document.querySelectorAll(".square")[0].style.background = "white"
-                        document.querySelectorAll(".square")[1].style.background = "white"
-                        document.querySelectorAll(".square")[2].style.background = "white"
-                        document.querySelectorAll(".square")[3].style.background = "white"
-                        document.querySelectorAll(".square")[4].style.background = "white"
-                        document.querySelectorAll(".square")[5].style.background = "white"
-                        document.querySelectorAll(".square")[6].style.background = "white"
-                        document.querySelectorAll(".square")[7].style.background = "white"
-                        document.querySelectorAll(".square")[8].style.background = "white"
-})
 let clickCount = 1
 function makeGrid() {
     for (i = 1; i < 10; i++) {
@@ -33,85 +25,115 @@ function makeGrid() {
         createDiv.addEventListener('click', function(evt) {
             if (clickCount % 2 == 1 && evt.target.style.background !== "blue" && evt.target.style.background !== "red") {
                 clickCount++;
+                console.log(clickCount)
                 evt.target.style.background = "blue";
                 let squareCheck = document.querySelectorAll(".square")
                 function gameCheckOne() {
                     if (squareCheck[0].style.background == "blue" && squareCheck[1].style.background == "blue" && squareCheck[2].style.background == "blue") {
-                        clearBoard()
-                        document.querySelector("h2").innerHTML = "PLAYER ONE WINS!!"
+                        clearBoard();
+                        document.querySelector("h2").innerHTML = "PLAYER ONE WINS!!\nPlayer One Start the game"
                         document.querySelector("h2").style.color = "blue"
+                        return;
                     } else if (squareCheck[3].style.background == "blue" && squareCheck[4].style.background == "blue" && squareCheck[5].style.background == "blue") {
-                        clearBoard()
-                        document.querySelector("h2").innerHTML = "PLAYER ONE WINS!!"
+                        clearBoard();
+                        document.querySelector("h2").innerHTML = "PLAYER ONE WINS!!\nPlayer One Start the game"
                         document.querySelector("h2").style.color = "blue"
+                        return;
                     } else if (squareCheck[6].style.background == "blue" && squareCheck[7].style.background == "blue" && squareCheck[8].style.background == "blue") {
-                        clearBoard()
-                        document.querySelector("h2").innerHTML = "PLAYER ONE WINS!!"
+                        clearBoard();
+                        document.querySelector("h2").innerHTML = "PLAYER ONE WINS!!\nPlayer One Start the game"
                         document.querySelector("h2").style.color = "blue"
+                        return;
                     } else if (squareCheck[0].style.background == "blue" && squareCheck[4].style.background == "blue" && squareCheck[8].style.background == "blue") {
-                        clearBoard()
-                        document.querySelector("h2").innerHTML = "PLAYER ONE WINS!!"
+                        clearBoard();
+                        document.querySelector("h2").innerHTML = "PLAYER ONE WINS!!\nPlayer One Start the game"
                         document.querySelector("h2").style.color = "blue"
+                        return;
                     } else if (squareCheck[2].style.background == "blue" && squareCheck[4].style.background == "blue" && squareCheck[6].style.background == "blue") {
-                        clearBoard()
-                        document.querySelector("h2").innerHTML = "PLAYER ONE WINS!!"
+                        clearBoard();
+                        document.querySelector("h2").innerHTML = "PLAYER ONE WINS!!\nPlayer One Start the game"
                         document.querySelector("h2").style.color = "blue"
+                        return;
                     } else if (squareCheck[0].style.background == "blue" && squareCheck[3].style.background == "blue" && squareCheck[6].style.background == "blue") {
-                        clearBoard()
-                        document.querySelector("h2").innerHTML = "PLAYER ONE WINS!!"
+                        clearBoard();
+                        document.querySelector("h2").innerHTML = "PLAYER ONE WINS!!\nPlayer One Start the game"
                         document.querySelector("h2").style.color = "blue"
-                    } else if (squareCheck[1].style.background == "blue" && squarCheck[4].style.background == "blue" && squareCheck[7].style.background == "blue") {
-                        clearBoard()
-                        document.querySelector("h2").innerHTML = "PLAYER ONE WINS!!"
+                        return;
+                    } else if (squareCheck[1].style.background == "blue" && squareCheck[4].style.background == "blue" && squareCheck[7].style.background == "blue") {
+                        clearBoard();
+                        document.querySelector("h2").innerHTML = "PLAYER ONE WINS!!\nPlayer One Start the game"
                         document.querySelector("h2").style.color = "blue"
+                        return;
                     } else if (squareCheck[2].style.background == "blue" && squareCheck[5].style.background == "blue" && squareCheck[8].style.background == "blue") {
-                        clearBoard()
-                        document.querySelector("h2").innerHTML = "PLAYER ONE WINS!!"
+                        clearBoard();
+                        document.querySelector("h2").innerHTML = "PLAYER ONE WINS!!\nPlayer One Start the game"
                         document.querySelector("h2").style.color = "blue"
-                    } else {
+                        return;
+                    } 
+                    else if (clickCount == 10) {
+                        clearBoard();
+                        document.querySelector("h2").innerHTML = "TIE!"
+                        document.querySelector("h2").style.color = "black"
+                    } 
+                    else {
                         document.querySelector("h2").innerHTML = "Player Two (RED)"
                         document.querySelector("h2").style.color = "red"
                     }
                 }
                 gameCheckOne()
             } else if (clickCount % 2 == 0 && evt.target.style.background !== "blue" && evt.target.style.background !== "red") {
-                clickCount++;
+                clickCount++
+                console.log(clickCount)
                 evt.target.style.background = "red";
                 let squareCheck = document.querySelectorAll(".square")
                 function gameCheckTwo() {
                     if (squareCheck[0].style.background == "red" && squareCheck[1].style.background == "red" && squareCheck[2].style.background == "red") {
-                        clearBoard()
-                        document.querySelector("h2").innerHTML = "PLAYER TWO WINS!!"
+                        clearBoard();
+                        document.querySelector("h2").innerHTML = "PLAYER TWO WINS!!\nPlayer One Start the game"
                         document.querySelector("h2").style.color = "red"
+                        return;
                     } else if (squareCheck[3].style.background == "red" && squareCheck[4].style.background == "red" && squareCheck[5].style.background == "red") {
-                        clearBoard()
-                        document.querySelector("h2").innerHTML = "PLAYER TWO WINS!!"
+                        clearBoard();
+                        document.querySelector("h2").innerHTML = "PLAYER TWO WINS!!\nPlayer One Start the game"
                         document.querySelector("h2").style.color = "red"
+                        return;
                     } else if (squareCheck[6].style.background == "red" && squareCheck[7].style.background == "red" && squareCheck[8].style.background == "red") {
-                        clearBoard()
-                        document.querySelector("h2").innerHTML = "PLAYER TWO WINS!!"
+                        clearBoard();
+                        document.querySelector("h2").innerHTML = "PLAYER TWO WINS!!\nPlayer One Start the game"
                         document.querySelector("h2").style.color = "red"
+                        return;
                     } else if (squareCheck[0].style.background == "red" && squareCheck[4].style.background == "red" && squareCheck[8].style.background == "red") {
-                        clearBoard()
-                        document.querySelector("h2").innerHTML = "PLAYER TWO WINS!!"
+                        clearBoard();
+                        document.querySelector("h2").innerHTML = "PLAYER TWO WINS!!\nPlayer One Start the game"
                         document.querySelector("h2").style.color = "red"
+                        return;
                     } else if (squareCheck[2].style.background == "red" && squareCheck[4].style.background == "red" && squareCheck[6].style.background == "red") {
-                        clearBoard()
-                        document.querySelector("h2").innerHTML = "PLAYER TWO WINS!!"
+                        clearBoard();
+                        document.querySelector("h2").innerHTML = "PLAYER TWO WINS!!\nPlayer One Start the game"
                         document.querySelector("h2").style.color = "red"
+                        return;
                     } else if (squareCheck[0].style.background == "red" && squareCheck[3].style.background == "red" && squareCheck[6].style.background == "red") {
-                        clearBoard()
-                        document.querySelector("h2").innerHTML = "PLAYER TWO WINS!!"
+                        clearBoard();
+                        document.querySelector("h2").innerHTML = "PLAYER TWO WINS!!\nPlayer One Start the game"
                         document.querySelector("h2").style.color = "red"
+                        return;
                     } else if (squareCheck[1].style.background == "red" && squareCheck[4].style.background == "red" && squareCheck[7].style.background == "red") {
-                        clearBoard()
-                        document.querySelector("h2").innerHTML = "PLAYER TWO WINS!!"
+                        clearBoard();
+                        document.querySelector("h2").innerHTML = "PLAYER TWO WINS!!\nPlayer One Start the game"
                         document.querySelector("h2").style.color = "red"
+                        return;
                     } else if (squareCheck[2].style.background == "red" && squareCheck[5].style.background == "red" && squareCheck[8].style.background == "red") {
-                        clearBoard()
-                        document.querySelector("h2").innerHTML = "PLAYER TWO WINS!!"
+                        clearBoard();
+                        document.querySelector("h2").innerHTML = "PLAYER TWO WINS!!\nPlayer One Start the game"
                         document.querySelector("h2").style.color = "red"
-                    } else {
+                        return;
+                    } 
+                    else if (clickCount == 10) {
+                        clearBoard();
+                        document.querySelector("h2").innerHTML = "TIE!"
+                        document.querySelector("h2").style.color = "black"
+                    } 
+                    else {
                         document.querySelector("h2").innerHTML = "Player One (BLUE)"
                         document.querySelector("h2").style.color = "blue"
                     }
@@ -122,3 +144,16 @@ function makeGrid() {
     )}
 }
 makeGrid()
+
+document.querySelector(".resetButton").addEventListener('click', function(){
+    document.querySelectorAll(".square")[0].style.background = "white"
+    document.querySelectorAll(".square")[1].style.background = "white"
+    document.querySelectorAll(".square")[2].style.background = "white"
+    document.querySelectorAll(".square")[3].style.background = "white"
+    document.querySelectorAll(".square")[4].style.background = "white"
+    document.querySelectorAll(".square")[5].style.background = "white"
+    document.querySelectorAll(".square")[6].style.background = "white"
+    document.querySelectorAll(".square")[7].style.background = "white"
+    document.querySelectorAll(".square")[8].style.background = "white"
+    location.reload()
+})
